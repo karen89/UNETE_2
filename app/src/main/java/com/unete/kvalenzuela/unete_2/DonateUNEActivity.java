fc225fb7335273181f5a30716667435408cc7ef9
Karen Valenzuela <karen89@users.noreply.github.com>
2018-04-30 19:30:04 -0500

    Initial commit GUI Funcional: Login, Signup

---
+++ app/src/main/java/com/unete/kvalenzuela/unete_2/DonateUNEActivity.java
@@ -0,0 +1,13 @@
+package com.unete.kvalenzuela.unete_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DonateUNEActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donate_une);
+    }
+}
